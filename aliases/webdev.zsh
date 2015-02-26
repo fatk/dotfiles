69ab2b8093511806e3a1168fde684d27f34424b4
Karl Fathi <karl.fathi@gmail.com>
2015-02-26 09:44:47 -0300

    - Replace: absolute plist file for brew prefixed ones. - New: osx script to forward port to 80 & 443

--- aliases/webdev.zsh
+++ aliases/webdev.zsh
@@ -1,12 +1,12 @@
 # Webdev
 
-alias start-nginx="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist"
-alias start-mysql="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
-alias start-php-fpm="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php56.plist"
+alias start-nginx="launchctl load $(brew --prefix nginx)/homebrew.mxcl.nginx.plist"
+alias start-mysql="launchctl load $(brew --prefix mysql)/homebrew.mxcl.mysql.plist"
+alias start-php-fpm="launchctl load $(brew --prefix php56)/homebrew.mxcl.php56.plist"
 
-alias stop-nginx="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist"
-alias stop-mysql="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
-alias stop-php-fpm="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.php56.plist"
+alias stop-nginx="launchctl unload $(brew --prefix nginx)/homebrew.mxcl.nginx.plist"
+alias stop-mysql="launchctl unload $(brew --prefix mysql)/homebrew.mxcl.mysql.plist"
+alias stop-php-fpm="launchctl unload $(brew --prefix php56)/homebrew.mxcl.php56.plist"
 
 alias start-webserver="start-nginx && start-mysql && start-php-fpm"
 alias stop-webserver="stop-nginx && stop-mysql && stop-php-fpm"
