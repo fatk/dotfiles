3a2d3fa857cb6ee2d80a5e967e4b3f3964e0abc6
Karl Fathi <karl.fathi@gmail.com>
2015-03-07 22:57:37 -0300

    - Fix: execute permissions - Add: Bootstrap file for creating trusted *.local.dev certificate - Add: Conditional alias declaration for web-server related aliases

--- aliases/webdev.zsh
+++ aliases/webdev.zsh
@@ -1,12 +1,22 @@
 # Webdev
 
+if [ -f $(brew --prefix nginx)/homebrew.mxcl.nginx.plist ]
+then
 alias start-nginx="launchctl load $(brew --prefix nginx)/homebrew.mxcl.nginx.plist"
-alias start-mysql="launchctl load $(brew --prefix mysql)/homebrew.mxcl.mysql.plist"
-alias start-php-fpm="launchctl load $(brew --prefix php56)/homebrew.mxcl.php56.plist"
-
 alias stop-nginx="launchctl unload $(brew --prefix nginx)/homebrew.mxcl.nginx.plist"
+fi
+
+if [ -f $(brew --prefix mysql)/homebrew.mxcl.mysql.plist ]
+then
+alias start-mysql="launchctl load $(brew --prefix mysql)/homebrew.mxcl.mysql.plist"
 alias stop-mysql="launchctl unload $(brew --prefix mysql)/homebrew.mxcl.mysql.plist"
+fi
+
+if [ -f $(brew --prefix php56)/homebrew.mxcl.php56.plist ]
+then
+alias start-php-fpm="launchctl load $(brew --prefix php56)/homebrew.mxcl.php56.plist"
 alias stop-php-fpm="launchctl unload $(brew --prefix php56)/homebrew.mxcl.php56.plist"
+fi
 
 alias start-webserver="start-nginx && start-mysql && start-php-fpm"
 alias stop-webserver="stop-nginx && stop-mysql && stop-php-fpm"
---
+++ bootstrap/create-dev-cert.sh
@@ -0,0 +1,17 @@
+sudo -v
+
+mkdir -p $(brew --prefix)/etc/nginx/ssl
+chmod o-rwx $(brew --prefix)/etc/nginx/ssl
+
+
+openssl req \
+  -new \
+  -newkey rsa:2048 \
+  -days 3650 \
+  -nodes \
+  -x509 \
+  -subj "/C=BR/ST=RJ/L=Rio De Janeiro/O=Organization/OU=$(whoami)/CN=*.local.dev" \
+  -keyout $(brew --prefix)/etc/nginx/ssl/\*.local.dev.key \
+  -out $(brew --prefix)/etc/nginx/ssl/\*.local.dev.crt
+
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $(brew --prefix)/etc/nginx/ssl/\*.local.dev.crt
