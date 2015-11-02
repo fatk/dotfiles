d434036b6537a20c9de1e7a7ec96112cf9b5f709
Karl Fathi <karl.fathi@gmail.com>
2015-11-02 14:04:19 -0200

    Add: php modules install

--- bootstrap/setup-web-server.sh
+++ bootstrap/setup-web-server.sh
@@ -1,5 +1,6 @@
 echo "Installing php 5.6..."
 brew install php56 --with-fpm --with-tidy
+brew install php56-imagick php56-intl php56-crypto php56-mcrypt php56-opcache php56-tidy php56-yaml
 echo "Installing mysql..."
 brew install mysql
 echo "Installing nginx..."
