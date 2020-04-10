de81230a0b9eb5fa6b0ba5535f5f6a161e0ae5ed
Karl Fathi <karl.fathi@gmail.com>
2020-04-10 14:49:05 -0300

    - Update: Create cert in a way that Chrome accepts

--- bootstrap/osx/create-dev-cert.sh
+++ bootstrap/osx/create-dev-cert.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-echo -n "User password required to add certificate to local trust store."
+echo "User password required to add certificate to local trust store."
 sudo -v
 
 openssl req \
-  -new \
-  -newkey rsa:2048 \
-  -days 3650 \
-  -nodes \
-  -x509 \
-  -subj "/C=BR/ST=RJ/L=Rio De Janeiro/O=Organization/OU=$(whoami)/CN=*.local.dev" \
-  -keyout local.dev.key \
-  -out local.dev.crt
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout local.dev.key \
+    -new \
+    -out local.dev.crt \
+    -subj "/C=BR/ST=RJ/L=Rio De Janeiro/O=Organization/OU=$(whoami)/CN=*.local.dev" \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /System/Library/OpenSSL/openssl.cnf \
+        <(printf '[SAN]\nsubjectAltName=DNS:*.local.dev')) \
+    -sha256 \
+    -days 3650
 
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain local.dev.crt
